494d2916788484bfee505b9604ff27281928fb09
Luis Felipe Gomez <luis.andrade18@hotmail.com>
2022-10-06 10:36:06 -0500

    Implemented initialSetup method

--- src/view/Main.java
+++ src/view/Main.java
@@ -1,6 +1,8 @@
 package view;
 
+import java.util.ArrayList;
 import java.util.Hashtable;
+import java.util.List;
 
 import model.Elevator;
 
@@ -10,12 +12,12 @@ public class Main {
 	/**
 	 * 
 	 */
-	private Hashtable<Integer, Integer> floorRequests;
+	private static Hashtable<Integer, Integer> floorRequests;
 	
 	/**
 	 * 
 	 */
-	private Elevator first_elevator;
+	private static Elevator first_elevator;
 	
 	
 	
@@ -23,7 +25,19 @@ public class Main {
 	 * 
 	 */
 	private static void initialSetup() {
+		floorRequests = new Hashtable<Integer, Integer>();
+		floorRequests.put(5, 2);
+		floorRequests.put(29, 10);
+		floorRequests.put(13, 1);
+		floorRequests.put(10, 1);
 		
+		List<Integer> list = new ArrayList<Integer>();
+		list.add(2);
+		list.add(10);
+		list.add(1);
+		list.add(1);
+		
+		first_elevator = new Elevator(4, list);
 	}
 	
 	
